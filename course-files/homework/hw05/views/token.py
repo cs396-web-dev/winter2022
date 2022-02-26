38fc5188855863b70d85611a77354363d76fb015
Sarah <vanwars@gmail.com>
2022-02-26 17:56:18 -0600

    removing space

--- course-files/homework/hw05/views/token.py
+++ course-files/homework/hw05/views/token.py
@@ -31,7 +31,7 @@ class RefreshTokenEndpoint(Resource):
         decoded_token = flask_jwt_extended.decode_token(refresh_token)
         exp_timestamp = decoded_token.get("exp")
         current_timestamp = datetime.timestamp(datetime.now(timezone.utc))
-        if  current_timestamp > exp_timestamp:
+        if current_timestamp > exp_timestamp:
             # token has expired:
             return Response(json.dumps({ 
                     "message": "refresh_token has expired"
